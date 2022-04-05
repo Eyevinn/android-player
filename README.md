2bb6f0d67f93fa9097b26ccecdc2918574d33b36
Ramanjot Kaur <79700214+iamramanjot@users.noreply.github.com>
2022-04-05 13:42:43 +0200

    Exoplayer (#2)

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# android-player
+# Description: #
+Eyevinn Andriod app to test HLS and MPEG-dash stream.
+
+## How to setup: ##
+- Download java version 1.8+.
+- Install Android studio IDE.
+- Set up the code in the Android studio.
+
+
+## How to run the app: ##
+- Set up the code in the Android studio.
+- Create a virtual device in AVD manager.
+- Run the application in virtual device.
+- Click on HLS or Mpeg-Dash button. 
+- Ensure that you have sample stream in the input box.
+- Click the Load button.
+- Selected stream will be played in the player.
+
+
+## How to run the tests: ##
+- Right click on the test and exeute it in Andriod studio.
+
+
+## Features: ## 
+- HLS Button to load sample HLS stream.
+- MPEG-DASH Button to load sample MPEG-DASH stream.
+- Load button to start the selected video stream.
+- User can add valid HLS/Mpeg-Dash video.
+
+
+![alt text](https://github.com/Eyevinn/android-player/blob/exoplayer-integration/Screenshot.png?raw=true)
---
+++ app/src/main/java/se/eyevinn/application/MainActivity.java
@@ -0,0 +1,133 @@
+package se.eyevinn.application;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+import org.jetbrains.annotations.NotNull;
+
+
+public class MainActivity extends AppCompatActivity implements VideoRendererEventListener {
+
+    public static final String HLS_URL = "https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8";
+    public static final String MPEG_DASH = "https://storage.googleapis.com/shaka-demo-assets/sintel-mp4-only/dash.mpd";
+    private static final String TAG = "MainActivity";
+    private SimpleExoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupPlayer();
+        loadButtonListener();
+        hlsButtonListener();
+        mpegDashButtonListener();
+    }
+
+    private void setupPlayer() {
+        if (player == null) {
+            DefaultTrackSelector trackSelector = new DefaultTrackSelector(this);
+            trackSelector.setParameters(
+                    trackSelector.buildUponParameters().setMaxVideoSizeSd());
+            player = new SimpleExoPlayer.Builder(this)
+                    .setTrackSelector(trackSelector)
+                    .build();
+        }
+        PlayerView playerView = findViewById(R.id.exo_player_view);
+        playerView.setPlayer(player);
+        playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+        EditText edtView = (EditText) findViewById(R.id.inputtext);
+        View.OnFocusChangeListener ofcListener = new MyFocusChangeListener();
+        edtView.setOnFocusChangeListener(ofcListener);
+        player.setPlayWhenReady(true); //run file/link when ready to play.
+    }
+
+    private void mpegDashButtonListener() {
+        Button mpeg_dashButton = (Button) findViewById(R.id.mpeg_dashButton);
+        mpeg_dashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.inputtext);
+                editText.setText(MPEG_DASH, TextView.BufferType.EDITABLE);
+                editText.clearFocus();
+            }
+        });
+    }
+
+    private void hlsButtonListener() {
+        Button hlsButton = (Button) findViewById(R.id.hlsButton);
+        hlsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.inputtext);
+                editText.setText(HLS_URL, TextView.BufferType.EDITABLE);
+                editText.clearFocus();
+            }
+        });
+    }
+
+    private void loadButtonListener() {
+        Button button = (Button) findViewById(R.id.loadButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.inputtext);
+                editText.clearFocus();
+                EditText inputText = (EditText) findViewById(R.id.inputtext);
+                String message = inputText.getText().toString();
+                playStreamInPlayer(message);
+            }
+        });
+    }
+
+    @NotNull
+    private void playStreamInPlayer(String url) {
+        if (url.endsWith("m3u8")) {
+            MediaItem mediaItem = new MediaItem.Builder()
+                    .setUri(url)
+                    .setMimeType(MimeTypes.APPLICATION_M3U8)
+                    .build();
+            player.setMediaItem(mediaItem);
+
+        } else if (url.endsWith("mpd")) {
+            MediaItem mediaItem = new MediaItem.Builder()
+                    .setUri(url)
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build();
+            player.setMediaItem(mediaItem);
+        } else {
+            Log.v(TAG, "Invalid url");
+        }
+        player.setPlayWhenReady(true);
+        player.seekTo(0, 0);
+        player.prepare();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy()...");
+        player.release();
+    }
+
+    private class MyFocusChangeListener implements View.OnFocusChangeListener {
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (v.getId() == R.id.inputtext && !hasFocus) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        }
+    }
+}
